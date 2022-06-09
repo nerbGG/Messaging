650b475a6c0ec801483a9f960841242880ad7bc8
nerbgg <nrbarthelemy@gmail.com>
2022-06-09 00:01:47 -0400

    getting messages from api

--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -1,12 +1,16 @@
+import React, {useEffect, useState} from 'react' 
 import WriteMessage from "./WriteMessage";
 import Messages from "./Messages";
+import ReactDOM from 'react-dom';
+
 // import ReactDOM from 'react-dom';
 
 const MessagePage=()=>{
+
     return (
         <div className="p-1 h-100 w-100 d-flex flex-column border bg-light">
             <Messages/>
-            <WriteMessage/>
+            <WriteMessage/>     
         </div>
     )
 }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,101 +1,93 @@
-import React, {useEffect} from 'react' 
+import React, {useEffect, useState} from 'react' 
 import Message from "./Message";
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const getMessages=()=>{
-   const messages  = [
-        {
-            text:"hello",
-            author:"Nel",
-            to:"krishna",
-            sent : true,     
-        },
-        {
-            text:"hello",
-            author:"Krishna",
-            to:"Nel",
-            sent :false,
-        },
-        {
-            text:"hello",
-            author:"Krishna",
-            to:"Nel",
-            sent:false,
-        },
-        {
-            text:"hello",
-            author:"Nel",
-            to:"krishna",
-            sent:true,
-        },
-        {
-            text:"i pooped just now",
-            author:"Nel",
-            to:"krishna",
-            sent:true,
-        },
-        {
-            text:"hello",
-            author:"Nel",
-            to:"krishna",
-            sent:true,
-        },
-        {
-            text:"i pooped just now",
-            author:"Nel",
-            to:"krishna",
-            sent:true,
-        },
-        {
-            text:"and showered",
-            author:"Nel",
-            to:"krishna",
-            sent:true,
-        },
-        {
-            text:"I am treating my sick brother",
-            author:"Krishna",
-            to:"Nel",
-            sent:false,
-        },
-        {
-            text:"I am treating my sick brother",
-            author:"Krishna",
-            to:"Nel",
-            sent:false,
-        },
-        {
-            text:"jsabdjhabsdjhbajdshbsjhadbjhasbdhjhbdjahbdjhhbasdjbhabsxbjhsabxssajhbxahbxhbbahxhbjashbxjhasbxjhsaxjhassxhbahbxjahsbxjhbsax",
-            author:"Krishna",
-            to:"Nel",
-            sent:false,
-        },
-        {
-            text:"one more for good measure, one more for good measure, one more for good measure, one more for good measure, one more for good measure",
-            author:"Nel",
-            to:"Krishna",
-            sent:true,
-        },
-    ]
-    return messages;
+const token= 'fe3800bbd7b04026ea8e0750babfdb9e784fd7cf'
+const current_user = {
+    "id":4,
+    "username":"nerb",
+}
+const scrolltoBottom=()=>{
+    let node  = document.getElementById("focus");
+    let e= ReactDOM.findDOMNode(node);  
+    e.scrollIntoView() ;
 }
 const Messages=()=>{
-    const messages = []
-    const messagesData = getMessages();
-    messagesData.forEach(message => {
-     messages.push(<Message author={message.author} text={message.text} sent={message.sent} /> );
-    });
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [prevValue, setPrevValue] = useState(null);
+    const [firstTimeloaded, setfirsttimeLoaded] = useState(false);
+    const messagesContainer = []
+    // both fetchmessages functions work
+    const fetchMessages = ()=> {
+        const url = 'http://127.0.0.1:8000/api/room/2/messages/'
+        axios.get(url)
+            .then(response=>{
+                // console.log(response.data);
+                setMessages(response.data);
+                setPrevValue(loading);
+                setLoading(false);
+                if(prevValue != loading){
+                    setfirsttimeLoaded(true);
+                }
+                // scrolltoBottom();
+            })
+            .catch(err=>{console.log(err)});
+    }
+    // const fetchMessages = async() =>{
+    //     const url = 'http://127.0.0.1:8000/api/room/2/messages/'
+    //     const response = await fetch(url, {
+    //         headers: {
+    //             'Authorization': `Token ${token}`
+    //         }
+    //     })
+    //     const data  = await response.json();
+    //     setMessages(data);
+    //     scrolltoBottom();
+    // }
+    // const fetchMessages = () => {
+    //     const url = 'http://127.0.0.1:8000/api/room/2/messages/'
+    //     const response = fetch(url, {
+    //             headers: {
+    //                 'Authorization': `Token ${token}`
+    //             }
+    //         }).then(response =>response.json())
+    //         .then(data=>{
+    //             if(data.length >0){
+    //                 setMessages(data);
+    //                 scrolltoBottom();
+    //             }
+    //         });
+    // }
     useEffect(()=>{
-        let node  = document.getElementById("focus");
-        let e= ReactDOM.findDOMNode(node);
-        // e.focus(); only works for 
-        e.scrollIntoView()    
-    });
+        // scrolltoBottom();
+        const interval = setInterval(function() {
+            fetchMessages();
+        }, 2000);
+        
+        if(firstTimeloaded){
+            scrolltoBottom();
+        }
+        // console.log("hello")
+        //  clearInterval(interval);
+    },[firstTimeloaded]);
+    // with empty brackets, the use effect will run only after it mounts the first time, 
+    //but qif we add a state, it call useeffect everytime that state changes
+    // console.log(messages);
+    if(!loading){
+        messages.forEach(message => {
+            messagesContainer.push(<Message author={message.author} text={message.text} sent={current_user.id === message.author.id ? true : false} /> );
+        });
+    }
+    
     return (
-        <div className="border p-1" style={{height:'90%', overflowY:'scroll', overflowX:'hidden'}}> 
-            {messages}
+        <div className="border p-1" style={{height:'90%', overflowY:'scroll', overflowX:'hidden'}}>     
+            {loading && (<h5>Getting Messages...</h5>)}
+            {!loading && messagesContainer}
             {/* empty div used to keep the lastest message in view when opening app*/}
            <div id="focus"></div>
+            {/* {!loading && scrolltoBottom()} */}
         </div>
     );
 }
